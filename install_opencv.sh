32214db943ef2a74aefa686da89e9df76ffc3285
Barz Xu <barz.xu@bitmain.com>
2018-01-24 21:43:39 +0800

    fix install shell

--- install_opencv.sh
+++ install_opencv.sh
@@ -9,6 +9,6 @@ mkdir build
 
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_OPENGL=ON -D WITH_V4L=ON -D WITH_LIBV4L=ON -D WITH_GSTREAMER=ON -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ -D INSTALL_PYTHON_EXAMPLE=ON -D PYTHON_EXECUTABLE=/usr/bin/python -D BUILD_PYTHON_SUPPORT=ON ..
-make -j8
+make -j4
 sudo make install
 sudo ldconfig
